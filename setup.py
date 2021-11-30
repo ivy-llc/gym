b90ea7823fb82df3dd3e2336e910107731d374e9
djl11 <daniel.lenton11@imperial.ac.uk>
2021-11-30 20:25:51 +0100

    modified setup.py so pypi long_description is derived from README.rst, but with all the raw html removed. Also changed so install_requires is inferred from requirements.txt.

--- setup.py
+++ setup.py
@@ -14,29 +14,51 @@
 # limitations under the License..
 # ==============================================================================
 import setuptools
+from pathlib import Path
+from distutils.core import setup
 
-setuptools.setup(
+
+def _strip(line):
+    return line.split(' ')[0].split('#')[0].split(',')[0]
+
+def is_html(line):
+    line_squashed = line.replace(' ', '')
+    if not line_squashed:
+        return False
+    if line_squashed[0] == '<' and line_squashed[-1] == '>':
+        return True
+    return False
+
+def is_raw_block(line):
+    line_squashed = line.replace(' ', '')
+    if len(line_squashed) < 11:
+        return False
+    if line_squashed[-11:] == '..raw::html':
+        return True
+    return False
+
+
+this_directory = Path(__file__).parent
+lines = (this_directory / "README.rst").read_text().split('\n')
+lines = [line for line in lines if not (is_html(line) or is_raw_block(line))]
+long_description = '\n'.join(lines)
+
+
+setup(
     name='ivy-gym',
     version='1.1.6',
     author='Ivy Team',
     author_email='ivydl.team@gmail.com',
     description='Fully differentiable reinforcement learning environments, written in Ivy.',
-    long_description="""# What is Ivy Gym?\n\nIvy Gym opens the door for intersectional research between supervised
-    learning (SL), reinforcement learning (RL), and trajectory optimization (TO), by implementing RL environments in a
-    fully differentiable manner.\n\n
-    Specifically, Ivy gym provides differentiable implementations of the classic control tasks from OpenAI Gym, as well
-    as a new Swimmer task, which illustrates the simplicity of creating new tasks using Ivy. The differentiable nature
-    of the environments means that the cumulative reward can be directly optimized for in a supervised manner, without
-    need for reinforcement learning, which is the de facto approach for optimizing cumulative rewards. Ivy currently
-    supports Jax, TensorFlow, PyTorch, MXNet and Numpy. Check out the [docs](https://ivy-dl.org/gym) for more info!""",
-    long_description_content_type='text/markdown',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
     url='https://ivy-dl.org/gym',
     project_urls={
         'Docs': 'https://ivy-dl.org/gym/',
         'Source': 'https://github.com/ivy-dl/gym',
     },
     packages=setuptools.find_packages(),
-    install_requires=['gym', 'GLU', 'pyglet'],
+    install_requires=[_strip(line) for line in open('requirements.txt', 'r')],
     classifiers=['License :: OSI Approved :: Apache Software License'],
     license='Apache 2.0'
 )
