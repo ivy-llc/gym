f6e9fde8d1b92fbbc87ff954e5aacfc1203dfda5
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-06-17 21:28:36 +0800

    fix tests helpers import

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from typing import Dict
 
 # local
 import ivy
-import ivy_tests.test_ivy.helpers as helpers
+from ivy_tests.test_ivy import helpers
 
 
 FW_STRS = ['numpy', 'jax', 'tensorflow', 'torch', 'mxnet']
