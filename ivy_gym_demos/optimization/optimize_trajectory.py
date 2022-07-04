73b6200b3cc805e9294a5209b1202adebaf77b19
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-07-04 19:44:11 +0800

    try making `initial_state` native

--- ivy_gym_demos/optimization/optimize_trajectory.py
+++ ivy_gym_demos/optimization/optimize_trajectory.py
@@ -6,6 +6,7 @@ import numpy as np
 
 
 def loss_fn(env, initial_state, logits_in):
+    initial_state = ivy.to_native(initial_state, nested=True)
     obs = env.set_state(initial_state)
     score = ivy.array([0.])
     for logs_ in ivy.unstack(logits_in, axis=0):
