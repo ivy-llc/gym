1c97696357f60004b9695bc2e3cea0a0baa9612a
juliagsy <67888047+juliagsy@users.noreply.github.com>
2022-07-12 15:27:43 +0800

    revert

--- ivy_gym_demos/optimization/optimize_trajectory.py
+++ ivy_gym_demos/optimization/optimize_trajectory.py
@@ -7,7 +7,7 @@ import numpy as np
 
 def loss_fn(env, initial_state, logits_in):
     env.set_state(initial_state)
-    score = ivy.native_array([0.])
+    score = ivy.array([0.])
     for logs_ in ivy.unstack(logits_in, axis=0):
         ac = ivy.tanh(logs_)
         rew = env.step(ac)[1]
