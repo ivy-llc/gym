68cde2eb7acf39905b9eaaca9a34ba33ca213209
Kareem Morsy <kreem.morsy@hotmail.com>
2023-08-15 19:07:56 +0300

    Change `lets-unify.ai` reference to `unify.ai`

--- docs/partial_conf.py
+++ docs/partial_conf.py
@@ -3,6 +3,6 @@ from docs.conf import html_theme_options
 
 html_theme_options["switcher"][
     "json_url"
-] = "https://lets-unify.ai/docs/versions/gym.json"
+] = "https://unify.ai/docs/versions/gym.json"
 
 repo_name = "gym"
